08d41c1167aee4de04a2a05fdd519ad7d2a2cc1f
ZeroDayRNA <skinekopia@gmail.com>
2021-08-10 00:05:53 -0700

    Fixed block texturing issues, still need to fix inventory texutres

--- src/main/java/thrice/cursed/ThriceCursed.java
+++ src/main/java/thrice/cursed/ThriceCursed.java
@@ -3,6 +3,7 @@ package thrice.cursed;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
@@ -17,14 +18,23 @@ public class ThriceCursed implements ModInitializer {
     public static final Item TEST_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
     public static final Block COLDISH_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
     public static final Block TEST_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+    public static final Block TEST_BLOCK_2 = new Block(FabricBlockSettings.of(Material.WOOD).strength(4.0f));
+
+
 
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("tcursed", "colidsh_item"), TEST_ITEM);
+
         Registry.register(Registry.BLOCK, new Identifier("tcursed", "coldish_block"), COLDISH_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("tcursed", "coldish_block"), new BlockItem(COLDISH_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
+
         Registry.register(Registry.BLOCK, new Identifier("tcursed","test_block"), TEST_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("tcursed","test_block"), new BlockItem(TEST_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
+
+        Registry.register(Registry.BLOCK, new Identifier("tcursed","test_block_2"), TEST_BLOCK_2);
+        Registry.register(Registry.ITEM, new Identifier("tcursed","test_block_2"), new BlockItem(TEST_BLOCK_2, new FabricItemSettings().group(ItemGroup.MISC)));
+
         System.out.println("Hello Fabric world!");
     }
 }
