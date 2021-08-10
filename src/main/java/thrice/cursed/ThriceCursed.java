8d25f7a0a9e3e1f6ac350ae7d45abefc9f1fd97a
ZeroDayRNA <skinekopia@gmail.com>
2021-08-09 23:00:11 -0700

    Added test block for texture troubleshooting

--- src/main/java/thrice/cursed/ThriceCursed.java
+++ src/main/java/thrice/cursed/ThriceCursed.java
@@ -16,12 +16,15 @@ public class ThriceCursed implements ModInitializer {
 
     public static final Item TEST_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
     public static final Block COLDISH_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+    public static final Block TEST_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("tcursed", "colidsh_iten"), TEST_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("tcursed", "colidsh_item"), TEST_ITEM);
         Registry.register(Registry.BLOCK, new Identifier("tcursed", "coldish_block"), COLDISH_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("tcursed", "coldish_block"), new BlockItem(COLDISH_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier("tcursed","test_block"), TEST_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("tcursed","test_block"), new BlockItem(TEST_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
         System.out.println("Hello Fabric world!");
     }
 }
