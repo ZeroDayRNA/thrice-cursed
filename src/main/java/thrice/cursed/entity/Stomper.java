a7000dfa0b5571378f7ef63a3831d6317ddbfcc6
ZeroDayRNA <skinekopia@gmail.com>
2021-08-10 22:27:20 -0700

    added stomper class stub

---
+++ src/main/java/thrice/cursed/entity/Stomper.java
@@ -0,0 +1,13 @@
+package thrice.cursed.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.world.World;
+
+public class Stomper extends ZombieEntity {
+
+    public Stomper(EntityType<? extends ZombieEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+}
